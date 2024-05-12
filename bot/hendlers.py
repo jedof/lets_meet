2ebfa8b3f27e14fc6fb1f44bdc5c411074795b05
Jedof <kravets10000@gmail.com>
2024-05-12 20:53:37 +0300

    registration

--- bot/hendlers.py
+++ bot/hendlers.py
@@ -11,19 +11,60 @@ import keyboards
 router = Router()
 
 
-async def registration(message, session):
+class Registration(StatesGroup):
+    name = State()
+    age = State()
+    city = State()
+    photo = State()
+
+
+@router.message(Registration.name, F.text)
+async def get_name(message: types.Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Введи возраст:")
+    await state.set_state(Registration.age)
+
+
+@router.message(Registration.age, F.text)
+async def get_age(message: types.Message, state: FSMContext):
+    try:
+        age = float(message.text)
+        if age % 1 == 0:
+            await state.update_data(age=age)
+            await message.answer("Введи город:")
+            await state.set_state(Registration.city)
+        else:
+            await message.answer("Введи число без дроби")
+    except ValueError:
+        await message.answer("Введи число")
     
-    await db.db_add_user(session, message)
-    await session.commit()
-    
+
+@router.message(Registration.city, F.text)
+async def get_city(message: types.Message, state: FSMContext):
+    await state.update_data(city=message.text)
+    await message.answer("Отправь фото")
+    await state.set_state(Registration.photo)
+
+
+@router.message(Registration.photo, F.photo)
+async def get_photo(message: types.Message, state: FSMContext):
+    await message.answer("Поздравляю ты зарегистрировался")
+    await registration(state, message)
+
+
+async def registration(state: FSMContext, message: types.Message):
+    async with get_db_session() as session:
+        await db.db_add_user(sessioт, state, message)
+        await session.commit()
 
 
 @router.message(Command("start"))
-async def start(message: types.Message):
-    async with get_db_session() as session:
-        user_info = await db.db_get_user_data(session, message.from_user.id)
-        if not user_info:
-            await registration(message, session)
+async def start(message: types.Message, state: FSMContext):
+    user_info = await db.db_get_user_data(session, message.from_user.id)
+    if not user_info:
+        await state.clear()
+        await message.answer("Привет! Зарегестрируйся.\nВведи имя:")
+        await state.set_state(Registration.name)
     #         state = "nick"
     #         await message.answer("Напиши свое имя")
     #     elif not user_info.gender:
